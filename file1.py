ab7597e33bf6dae3eeb18625ce76fc43aed9f95d
m4rCs1l <ie.belokonn@gmail.com>
2022-09-24 22:05:35 +0300

    added new features

--- file1.py
+++ file1.py
@@ -1,5 +1,7 @@
 def get_date():
-	date = input('Введите дату текущего дня (формат: день.месяц.год): ')
-	clndr = date.split('.')
-	print(clndr)
+	day = input('Введите номер текущего дня: ')
+	month = input('Введите номер месяца: ')
+	year = input('Введите год: ')
+	date = print(f'Дата сегодняшнего дня: {day}.{month}.{year}')
+	print(date)
 get_date()
